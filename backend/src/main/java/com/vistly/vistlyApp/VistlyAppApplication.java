0fbfe4518961a6894577005a3fe9bb1146c786cf
graydon-whitespark <graydon@whitespark.ca>
2025-03-23 21:12:20 -0600

    i think it's running again

---
+++ backend/src/main/java/com/vistly/vistlyApp/VistlyAppApplication.java
@@ -0,0 +1,13 @@
+package com.vistly.vistlyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VistlyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VistlyAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vistly/vistlyApp/VistlyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vistly.vistlyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VistlyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

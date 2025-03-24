0cbb1f02d1c950ec392c21434f351f838f2e4d1a
graydon-whitespark <graydon@whitespark.ca>
2025-03-23 21:10:11 -0600

    try restarting backend

--- backend/src/main/java/com/vistly/vistlyApp/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vistly.vistlyApp.repository;
-
-import com.vistly.vistlyApp.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-}

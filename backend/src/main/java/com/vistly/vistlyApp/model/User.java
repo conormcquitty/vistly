5ea8c033c24eb6cf060a04b1c58e3790e2ace0a8
graydon-whitespark <graydon@whitespark.ca>
2025-03-25 17:55:45 -0600

    lombok

--- backend/src/main/java/com/vistly/vistlyApp/model/User.java
+++ backend/src/main/java/com/vistly/vistlyApp/model/User.java
@@ -1,25 +1,19 @@
 package com.vistly.vistlyApp.model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private long id;
     private String firstName;
     private String lastName;
     private String emailId;
-
-    public User(long id, String firstName, String lastName, String emailId) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.emailId = emailId;
-    }
-
-    public User() {
-    }
 
 }

cd785da6d4dc45c9b95311308f97f259ba3792a0
graydon-whitespark <graydon@whitespark.ca>
2025-03-28 14:54:39 -0600

    add favicon

--- frontend/src/app/users/page.tsx
+++ frontend/src/app/users/page.tsx
@@ -5,15 +5,13 @@ import { useState, useEffect } from 'react';
 export default function Home() {
     const [userData, setUserData] = useState(null);
 
-    useEffect(() => {
-        const fetchUsers = async () => {
-            const response = await fetch('http://localhost:8080/api/v1/users');
-            const data = await response.json();
-            setUserData(data);
-        };
+    const fetchUsers = async () => {
+        const response = await fetch('http://localhost:8080/api/v1/users');
+        const data = await response.json();
+        setUserData(data);
+    };
 
-        fetchUsers();
-    }, []);
+    useEffect(() => fetchUsers(), []);
 
     return (
         <>

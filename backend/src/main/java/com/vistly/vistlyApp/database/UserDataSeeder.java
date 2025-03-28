7078d914cef40f12c4c08ba2c88342340574acfb
graydon-whitespark <graydon@whitespark.ca>
2025-03-28 12:28:21 -0600

    update seeder data

--- backend/src/main/java/com/vistly/vistlyApp/database/UserDataSeeder.java
+++ backend/src/main/java/com/vistly/vistlyApp/database/UserDataSeeder.java
@@ -12,6 +12,7 @@ import java.util.List;
 public class UserDataSeeder {
 
     private final UserRepository userRepository;
+    private static final String PROFILE_IMAGE_BASE_URL = "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fres.cloudinary.com%2Fglide%2Fimage%2Fupload%2Fonboarding_img%2F";
 
     public UserDataSeeder(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -24,51 +25,51 @@ public class UserDataSeeder {
             if (userRepository.count() == 0) {
                 List<UserEntity> users = List.of(
                         createUser("Ava", "Wilder", "ava_wilder@example.com",
-                                "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fres.cloudinary.com%2Fglide%2Fimage%2Fupload%2Fonboarding_img%2F0195b5f4-7e36-7ff3-a47e-f5caad3eae39",
+                                PROFILE_IMAGE_BASE_URL + "0195b5f4-7e36-7ff3-a47e-f5caad3eae39",
                                 "Vancouver", "BC", "Canada"),
 
                         createUser("Cameron", "Lee", "cameron_lee@example.com",
-                                "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fres.cloudinary.com%2Fglide%2Fimage%2Fupload%2Fonboarding_img%2F0195b5f4-7e44-7ff3-a47f-1cbee82febdb",
+                                PROFILE_IMAGE_BASE_URL + "0195b5f4-7e44-7ff3-a47f-1cbee82febdb",
                                 "Toronto", "ON", "Canada"),
 
                         createUser("Ethan", "Bishop", "ethan_bishop@example.com",
-                                "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fres.cloudinary.com%2Fglide%2Fimage%2Fupload%2Fonboarding_img%2F0195b5f4-7e35-7ff3-a47e-ea1cc3c16931",
+                                PROFILE_IMAGE_BASE_URL + "0195b5f4-7e35-7ff3-a47e-ea1cc3c16931",
                                 "Seattle", "WA", "United States"),
 
                         createUser("Ethan", "Kim", "ethan_kim@example.com",
-                                "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fres.cloudinary.com%2Fglide%2Fimage%2Fupload%2Fonboarding_img%2F0195b5f4-7e32-7ff3-a47e-da143a3a741f",
+                                PROFILE_IMAGE_BASE_URL + "0195b5f4-7e32-7ff3-a47e-da143a3a741f",
                                 "Whistler", "BC", "Canada"),
 
                         createUser("Giovanni", "Mendez", "giovanni_mendez@example.com",
-                                "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fres.cloudinary.com%2Fglide%2Fimage%2Fupload%2Fonboarding_img%2F0195b5f4-7e39-7ff3-a47f-0749f663da66",
+                                PROFILE_IMAGE_BASE_URL + "0195b5f4-7e39-7ff3-a47f-0749f663da66",
                                 "Los Angeles", "CA", "United States"),
 
                         createUser("Luna", "Rodriguez", "luna_rodriguez@example.com",
-                                "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fres.cloudinary.com%2Fglide%2Fimage%2Fupload%2Fonboarding_img%2F0195b5f4-7e30-7ff3-a47e-d1b6053b7b4c",
+                                PROFILE_IMAGE_BASE_URL + "0195b5f4-7e30-7ff3-a47e-d1b6053b7b4c",
                                 "Calgary", "AB", "Canada"),
 
                         createUser("Martha", "Lomax", "martha_lomax@example.com",
-                                "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fres.cloudinary.com%2Fglide%2Fimage%2Fupload%2Fonboarding_img%2F0195b5f4-7e38-7ff3-a47e-f8cc54aa7f0a",
+                                PROFILE_IMAGE_BASE_URL + "0195b5f4-7e38-7ff3-a47e-f8cc54aa7f0a",
                                 "Toronto", "ON", "Canada"),
 
                         createUser("Mateo", "Rios", "mateo_rios@example.com",
-                                "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fres.cloudinary.com%2Fglide%2Fimage%2Fupload%2Fonboarding_img%2F0195b5f4-7e38-7ff3-a47e-f8cc54aa7f0a",
+                                PROFILE_IMAGE_BASE_URL + "0195b5f4-7e38-7ff3-a47e-f8cc54aa7f0a",
                                 "Toronto", "ON", "Canada"),
 
                         createUser("Olivia", "Rodriguez", "olivia_rodriguez@example.com",
-                                "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fres.cloudinary.com%2Fglide%2Fimage%2Fupload%2Fonboarding_img%2F0195b5f4-7e2d-7ff3-a47e-c28e33aacc73",
+                                PROFILE_IMAGE_BASE_URL + "0195b5f4-7e2d-7ff3-a47e-c28e33aacc73",
                                 "Boston", "MA", "United States"),
 
                         createUser("Rebecca", "Vasquez", "rebecca_vasquez@example.com",
-                                "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fres.cloudinary.com%2Fglide%2Fimage%2Fupload%2Fonboarding_img%2F0195b5f4-7e32-7ff3-a47e-e09981b1103a",
+                                PROFILE_IMAGE_BASE_URL + "0195b5f4-7e32-7ff3-a47e-e09981b1103a",
                                 "San Francisco", "CA", "United States"),
 
                         createUser("Ronald", "Chen", "ronald_chen@example.com",
-                                "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fres.cloudinary.com%2Fglide%2Fimage%2Fupload%2Fonboarding_img%2F0195b5f4-7e2f-7ff3-a47e-c96cc87533d3",
+                                PROFILE_IMAGE_BASE_URL + "0195b5f4-7e2f-7ff3-a47e-c96cc87533d3",
                                 "Manhattan", "NYC", "United States"),
 
                         createUser("Sofia", "Patel", "sofia_patel@example.com",
-                                "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fres.cloudinary.com%2Fglide%2Fimage%2Fupload%2Fonboarding_img%2F0195b5f4-7e42-7ff3-a47f-118b4b4b650b",
+                                PROFILE_IMAGE_BASE_URL + "0195b5f4-7e42-7ff3-a47f-118b4b4b650b",
                                 "Paramus", "NJ", "United States")
                 );
 

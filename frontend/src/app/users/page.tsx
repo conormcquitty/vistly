0b63818b636283c5c9ec1d216a434da7171ec2e3
graydon-whitespark <graydon@whitespark.ca>
2025-03-30 14:47:53 -0600

    .

--- frontend/src/app/users/page.tsx
+++ frontend/src/app/users/page.tsx
@@ -3,25 +3,21 @@
 import { useState, useEffect } from 'react';
 
 export default function Home() {
-    const [isLoading, setLoading] = useState(true)
-    const [userData, setUserData] = useState(null);
+    const [users, setUsers] = useState(null);
 
     useEffect(() => {
-        const fetchUser = async () => {
+        const fetchUsers = async () => {
             const response = await fetch('http://localhost:8080/api/v1/users');
             const data = await response.json();
-            setUserData(data);
-            setLoading(false);
+            setUsers(data);
         };
 
-        fetchUser();
+        fetchUsers();
     }, []);
-
-    if (isLoading) return <p>Loading...</p>
 
     return (
         <>
-            <pre>{JSON.stringify(userData, null, 2)}</pre>
+            <pre>{JSON.stringify(users, null, 2)}</pre>
         </>
     );
 }

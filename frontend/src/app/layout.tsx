f45c10fc8bb4ab2f402ddf595aeb03c893613374
graydon-whitespark <graydon@whitespark.ca>
2025-03-30 22:09:13 -0600

    next.js restructure

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "./ui/globals.css";
+import {roboto} from "./ui/fonts"
 
 export const metadata: Metadata = {
   title: "Vistly",
@@ -24,7 +14,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+
+      <body className={`${roboto.className} antialiased`}>
         {children}
       </body>
     </html>

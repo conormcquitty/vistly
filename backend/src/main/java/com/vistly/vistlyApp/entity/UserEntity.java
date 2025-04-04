f481c19d81c3300271228a605d735c971b6fc763
graydon-whitespark <graydon@whitespark.ca>
2025-04-04 12:19:57 -0600

    Extend the User Entity with authentication details

--- backend/src/main/java/com/vistly/vistlyApp/entity/UserEntity.java
+++ backend/src/main/java/com/vistly/vistlyApp/entity/UserEntity.java
@@ -2,6 +2,11 @@ package com.vistly.vistlyApp.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Setter
 @Getter
@@ -10,7 +15,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "users")
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,4 +29,34 @@ public class UserEntity {
     private String administrativeArea; // state/province
     private String country;
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
